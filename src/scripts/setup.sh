4f6c98c2c6f405a4426f8b69a9f146498ef5a6ca
Stefano Mazzone <smazzone@users.noreply.github.com>
2024-09-30 10:51:41 +0200

    Update setup.sh

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -5,7 +5,7 @@
 # ---------------------------
 
 # Install KinD
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = arm64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind --version
